129066348294ea78bffd497e74caa6627806eee2
Naresh-Babu-Nangineedi <nangineedinaresh@gmail.com>
2020-07-29 20:18:32 +0530

    completed

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('config');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+// app.use(cors());
+app.use(express.json());
+
+
+//const uri = process.env.ATLAS_URI;
+const uri = config.get("uri");
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongoose Database established successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+  }
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+});
